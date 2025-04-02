be764ce4ccce5989c4ca5780a1b0fda5804c82bf
Tanmay123-sp <83634606+Tanmay123-sp@users.noreply.github.com>
2025-04-02 12:59:52 +0530

    Update dbusclient.cpp

--- dbusclient.cpp
+++ dbusclient.cpp
@@ -12,20 +12,6 @@ dbusClient::dbusClient(QWidget *parent) : QWidget(parent), m_label(new QLabel(th
     setLayout(layout);
 
     // Connect to the input signal
-    // bool inputConnected = QDBusConnection::sessionBus().connect(
-    //     "com.example.InputService",
-    //     "/InputObject",
-    //     "com.example.InputService",
-    //     "inputReceived",
-    //     this, SLOT(onInputReceived(QString))
-    //     );
-
-    // if (!inputConnected) {
-    //     qCritical() << "Failed to connect to inputReceived signal:" << QDBusConnection::sessionBus().lastError().message();
-    // } else {
-    //     qDebug() << "Successfully connected to inputReceived signal.";
-    // }
-
     bool inputsConnected = QDBusConnection::sessionBus().connect(
         "com.example.InputService",
         "/InputObject",
@@ -39,30 +25,7 @@ dbusClient::dbusClient(QWidget *parent) : QWidget(parent), m_label(new QLabel(th
     } else {
         qDebug() << "Successfully connected to inputReceived signal.";
     }
-
-    // Connect to the amount signal
-//     bool amountConnected = QDBusConnection::sessionBus().connect(
-//         "com.example.InputService",
-//         "/InputObject",
-//         "com.example.InputService",
-//         "amountReceived",
-//         this, SLOT(onAmountReceived(QString))
-//         );
-
-//     if (!amountConnected) {
-//         qCritical() << "Failed to connect to amountReceived signal:" << QDBusConnection::sessionBus().lastError().message();
-//     } else {
-//         qDebug() << "Successfully connected to amountReceived signal.";
-//     }
 }
-
-// void dbusClient::onInputReceived(const QString &input) {
-//     m_label->setText("Received Name: " + input);
-// }
-
-// void dbusClient::onAmountReceived(const QString &amount) {
-//     m_amount->setText("Received Amount: " + amount);
-// }
 
 void dbusClient::onInputsReceived(const QString &input, const QString &amount)
 {
